3fc9c7c693baf2be14529c866c60c28c02eefba1
haolinwei <haolinwei@58.com>
2020-07-01 11:31:34 +0800

    Initial Commit

---
+++ WidgetDecorator/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WidgetDecorator
+//
+//  Created by Linwei Hao on 2020/7/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WidgetDecorator/WidgetDecoratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetDecoratorApp.swift
+//  WidgetDecorator
+//
+//  Created by Linwei Hao on 2020/7/1.
+//
+
+import SwiftUI
+
+@main
+struct WidgetDecoratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
